0d869e6fc6289c937d9e547f976475f77c5c1b76
WONG SHI QUAN <shiquan0327@gmail.com>
2025-06-03 15:55:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -93,8 +93,8 @@ def main():
 
     if st.button("Predict"):
     # Show personalized advice first
-        advice = advice_on_values(age, bmi, glucose)
-    for adv in advice:
+        advices = advice_on_values(age, bmi, glucose)
+    for adv in advices:
         st.info(adv)
 
     # Run prediction and get probability
